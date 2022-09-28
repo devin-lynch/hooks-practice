42a0b3d14a56ba01836b6f9146de96318b2407da
devin-lynch <devin.l.lynch@gmail.com>
2022-09-28 16:24:55 -0500

    Test grade set

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(1)
   const [user, setUser] = useState('Gabe')
+  const [isGabe, setIsGabe] = useState(true)
+  const [objUser, setObjUser] = useState({name: 'Gabe', location: 'Berkeley'})
+  const [finishedGrades, setFinishedGrades] = useState([55, 75, 100])
+  const [grades, setGrades] = useState([])
 
   const increaseCount = () => {
     setCount(count + 1)
@@ -12,14 +16,31 @@ function App() {
 
   const setToWeston = () => {
     setUser('Weston')
+    setIsGabe(false)
   }
 
   const setToApril = () => {
     setUser('April')
+    setIsGabe(false)
   }
 
   const setToGabe = () => {
     setUser('Gabe')
+    setIsGabe(true)
+  }
+
+  const setObjUserToWeston = () => {
+    setObjUser({...objUser, name: 'Weston'})
+  }
+  const setObjUserToApril = () => {
+    setObjUser({...objUser, name: 'April'})
+  }
+  const setObjUserToGabe = () => {
+    setObjUser({...objUser, name: 'Gabe'})
+  }
+
+  const addHundred = () => {
+    setFinishedGrades([...finishedGrades, 100])
   }
 
   // // add 1 to count every 5 seconds
@@ -31,11 +52,20 @@ function App() {
     <div className="App">
       <h1>The count is: {count}</h1>
       <button onClick={increaseCount}>Count up!</button>
-      <h2>The user is: {user}</h2>
+      <h2 style={{color: isGabe ? 'purple' : 'black'}}>The user is: {user}</h2>
       <button onClick={setToWeston}>Weston</button>
       <button onClick={setToApril}>April</button>
       <button onClick={setToGabe}>Gabe</button>
-
+      <h2>{objUser.name}</h2>
+      <h2>{objUser.location}</h2>
+      <button onClick={setObjUserToWeston}>SetObjUser Weston</button>
+      <button onClick={setObjUserToApril}>SetObjUser April</button>
+      <button onClick={setObjUserToGabe}>SetObjUser Gabe</button>
+      <h1>Grades:</h1>
+      {finishedGrades.map((grade, i) => {
+        return <p key={i}>{grade}</p>
+      })}
+      <button onClick={addHundred}>New 100</button>
     </div>
   );
 }
