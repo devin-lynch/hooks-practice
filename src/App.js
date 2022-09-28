c56f26d2bb72777bbafe0e512102a1ce1590c971
devin-lynch <devin.l.lynch@gmail.com>
2022-09-28 16:09:40 -0500

    Test setInterval

--- src/App.js
+++ src/App.js
@@ -3,20 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [value, setValue] = useState('hello')
-  
-  let setToHello = () => {
-    setValue('hello')
+  const [count, setCount] = useState(1)
+  const [user, setUser] = useState('Gabe')
+
+  const increaseCount = () => {
+    setCount(count + 1)
   }
 
-  let setToGoodbye = () => {
-    setValue('goodbye')
+  const setToWeston = () => {
+    setUser('Weston')
   }
+
+  const setToApril = () => {
+    setUser('April')
+  }
+
+  const setToGabe = () => {
+    setUser('Gabe')
+  }
+
+  // // add 1 to count every 5 seconds
+  // setInterval(() => {
+  //   setCount(count + 1)
+  // }, 5000)
+
   return (
     <div className="App">
-      <h1>{value}</h1>
-      <button onClick={setToHello}>Hello</button>
-      <button onClick={setToGoodbye}>Goodbye</button>
+      <h1>The count is: {count}</h1>
+      <button onClick={increaseCount}>Count up!</button>
+      <h2>The user is: {user}</h2>
+      <button onClick={setToWeston}>Weston</button>
+      <button onClick={setToApril}>April</button>
+      <button onClick={setToGabe}>Gabe</button>
+
     </div>
   );
 }
